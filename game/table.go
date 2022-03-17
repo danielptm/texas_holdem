be48ec2a334447540b992f268d5b85fd8f428e03
Daniel <danielptm@me.com>
2022-03-17 08:32:08 -0700

    retrigger

--- game/table.go
+++ game/table.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-type Game struct {
+type Table struct {
 	players   []Player
 	community []Card
 	button    Player
@@ -14,14 +14,14 @@ type Game struct {
 func Start() {
 	players := []Player{{name: "Daniel", chips: 100}, {name: "Computer", chips: 100}}
 	community := []Card{{suit: "♠️", value: "3"}, {suit: "❤️", value: "5"}, {suit: "♣️", value: "5"}}
-	var game = Game{players: players, community: community, button: players[0], pot: 13}
+	var game = Table{players: players, community: community, button: players[0], pot: 13}
 	game.Status()
 	//deck := GetDeck()
 	//deck = deck.Shuffle()
 	//fmt.Print(deck)
 }
 
-func (g Game) Status() {
+func (g Table) Status() {
 	fmt.Println("")
 	fmt.Println("------------------STATE----------------------")
 	fmt.Printf("Player: %s, Chips: %d", g.players[0].name, g.players[0].chips)
